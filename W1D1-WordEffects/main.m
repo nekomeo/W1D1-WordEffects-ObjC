dd162b55152cf96fa80c212cdac245018d09fefe
nekomeo <linh.tu545@gmail.com>
2017-05-29 16:29:16 -0700

    Initial Commit

---
+++ W1D1-WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W1D1-WordEffects
+//
+//  Created by Elle Ti on 2017-05-29.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
