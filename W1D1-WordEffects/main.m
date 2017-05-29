2c4d395a118ae759f6f17085cd9d03619ec909ba
nekomeo <linh.tu545@gmail.com>
2017-05-29 16:31:11 -0700

    Add options 1-3

--- W1D1-WordEffects/main.m
+++ W1D1-WordEffects/main.m
@@ -10,8 +10,44 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        
+        char inputChars[255];
+        char again;
+        int choice;
+        
+        printf("Which option would you like to pick?\n");
+        scanf("%c", &again);
+        
+        while (again == 'y')
+        {
+            char *again = "Please choose from the following";
+            switch (choice) {
+                case 1:
+                    NSLog(@"Input a string");
+                    fgets(inputChars, 255, stdin);
+                    NSString *uppercase = [inputChars uppercaseString];
+                    NSLog(@"%@", uppercase);
+                    break;
+                case 2:
+                    NSLog(@"Input a string");
+                    fgets(inputChars, 255, stdin);
+                    NSString *lowercase = [inputChars lowercaseString];
+                    NSLog(@"%@", lowercase);
+                    break;
+                case 3:
+                    NSLog(@"Enter a number");
+                    fgets(inputChars, 255, stdin);
+                    NSNumberFormatter *stringToNumber = [[NSNumberFormatter alloc] init];
+                    stringToNumber.numberStyle = NSNumberFormatterDecimalStyle;
+                    NSNumber *myNumber = [stringToNumber numberFromString:@"42"]; // Change this once you understand what's going on
+                    break;
+                case 4:
+                    NSLog(@"Enter a word");
+                default:
+                    break;
+            }
+        }
     }
     return 0;
 }
