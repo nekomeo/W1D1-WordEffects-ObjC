879f847f355da5c71b8ac1030e64cddf0e5b3d6a
nekomeo <linh.tu545@gmail.com>
2017-05-29 20:31:02 -0700

    Add character count

--- W1D1-WordEffects/main.m
+++ W1D1-WordEffects/main.m
@@ -10,106 +10,99 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+        // 255 Unit long array of characters
         char inputChars[255];
-        char again;
-        char play;
+        char play[255];
         int choice;
         
-        printf("Which option would you like to pick?\n");
-        scanf("%c", &choice);
+        printf("Would you like to pick? ");
+        fgets(play, 255, stdin);
+        strtok(play, "\n");
+        NSMutableString *convertedPlay = [NSMutableString stringWithUTF8String:play];
         
-        if (play == 'y')
+        
+        NSLog(@"Input a string");
+        fgets(inputChars, 255, stdin);
+        strtok(inputChars, "\n");
+        NSString *convertedChar = [NSString stringWithUTF8String:inputChars];
+        
+        while ([[convertedPlay lowercaseString]  isEqual: @"y"])
         {
-            NSLog(@"Uppercase");
-            NSLog(@"Lowercase");
-            NSLog(@"Numberize");
-            NSLog(@"Canadianize");
-            NSLog(@"Respond");
-            NSLog(@"De-Space-It");
-            scanf("", &choice);
+            NSLog(@"\n1. Uppercase\n2. Lowercase\n3. Numberize\n4. Canadianize\n5. Respond\n6. De-Space-It\n7. Word count");
+            scanf(" %d", &choice);
             
-//            char *again = "Please choose from the following";
-//            fgets(<#char *restrict#>, <#int#>, <#FILE *#>) // fgets the choice you want to pick
             switch (choice)
             {
                 case 1: // Uppercase word entered
-                NSLog(@"Input a string");
                 {
-                    fgets(inputChars, 255, stdin);
-                    NSString *convertedChar = [NSString stringWithUTF8String:inputChars];
                     NSString *uppercase = [convertedChar uppercaseString];
                     NSLog(@"%s uppercased is %@", inputChars, uppercase);
-                }
                     break;
+                }
                 case 2: // Lowercase word entered
                 {
-                    NSLog(@"Input a string");
-                    fgets(inputChars, 255, stdin);
-                    NSString *convertedChar = [NSString stringWithUTF8String:inputChars];
                     NSString *lowercase = [convertedChar lowercaseString];
                     NSLog(@"%s lowercased is %@", inputChars, lowercase);
-                }
                     break;
+                }
                 case 3: // Change number from string to int/float/etc.
                 {
-                    NSLog(@"Enter a number");
-                    fgets(inputChars, 255, stdin);
                     strtok(inputChars, "\n");
-                    NSString *convertedChar = [NSString stringWithUTF8String:inputChars];
                     NSNumberFormatter *stringToNumber = [[NSNumberFormatter alloc] init];
                     stringToNumber.numberStyle = NSNumberFormatterNoStyle;
                     NSNumber *myNumber = [stringToNumber numberFromString:convertedChar];
                     NSLog(@"The string %@ is converted to %@", convertedChar, myNumber);
-                }
                     break;
+                }
                 case 4: // Canadianize - add ", eh?" to the end of the input
                 {
-                    NSLog(@"Enter a string");
-                    fgets(inputChars, 255, stdin);
                     strtok(inputChars, "\n");
-                    NSMutableString *convertedChar = [NSString stringWithUTF8String:inputChars];
-                    NSMutableString *canadianize = [convertedChar stringByAppendingString:@", eh?"];
+                    NSString *canadianize = [convertedChar stringByAppendingString:@", eh?"];
                     NSLog(@"String Canadianized is: %@", canadianize);
-                }
                     break;
+                }
                 case 5: // Respond to a question or statement
                 {
-                    NSLog(@"Enter a string:");
-                    fgets(inputChars, 255, stdin);
-                    NSString *sentence = [NSString stringWithUTF8String:inputChars];
-                    if ([sentence containsString:@"?"])
+                    if ([convertedChar containsString:@"?"])
                     {
                         NSLog(@"I don't know");
                     }
-                    else if ([sentence containsString:@"!"])
+                    else if ([convertedChar containsString:@"!"])
                     {
                         NSLog(@"Whoa, calm down!");
                     }
                     else
                     {
-                        NSLog(@"%@", sentence);
+                        NSLog(@"%@", convertedChar);
                     }
+                    break;
                 }
                 case 6: // De-Space-It - Replace " " with "-"
                 {
-                    NSLog(@"Enter a sentence:");
-                    fgets(inputChars, 255, stdin);
-                    NSString *sentence = [NSString stringWithUTF8String:inputChars];
-                    NSString *newSentence = [sentence stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+                    NSString *newSentence = [convertedChar stringByReplacingOccurrencesOfString:@" " withString:@"-"];
                     NSLog(@"Without spaces: %@", newSentence);
+                    break;
+                }
+                case 7: // Character count - Count the number of characters in the string
+                {
+                    NSUInteger *characterCount = [convertedChar length];
+                    NSLog(@"Number of characters is: %lu", characterCount);
                 }
                 default:
                     break;
             }
-////            printf("Would you like to play again? (y/n)\n");
-////            scanf(" &c", again);
+//            NSLog(@"\nWould you like to play again? (y/n)");
+            
+            
+            char playA[255];
+            
+            fgets(playA, 255, stdin);
+            strtok(playA, "\n");
+//            convertedPlay = [NSMutableString stringWithUTF8String:playA];
+            
+            
+            
         }
     }
-    
-//            char name[10];
-//            printf("Who are you? ");
-//            fgets(name, 10, stdin);
-//            printf("Glad to meet you %s\n", name);
     return 0;
 }
